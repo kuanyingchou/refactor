c6208dcfa380cc5e917972488e09c161b3a03e30
KUAN-YING CHOU <kuanyingchou@gmail.com>
2018-11-25 17:15:51 +0800

    change declaration of amountFor

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function statement (invoice, plays) {
 
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
     case "tragedy":
       result = 40000;
       if (aPerformance.audience > 30) {
@@ -17,7 +17,7 @@ function statement (invoice, plays) {
       result += 300 * aPerformance.audience;
       break;
     default:
-        throw new Error(`unknown type: ${play.type}`);
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
     return result;
   }
@@ -34,7 +34,7 @@ function statement (invoice, plays) {
                           minimumFractionDigits: 2 }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
 
     // add volume credits
