b04a1cacede14a54e7dee7cb47ef76e69dfe4787
Kuan-Ying Chou <kuanyingchou@gmail.com>
2018-11-25 14:23:36 +0800

    initial commit

---
+++ index.test.js
@@ -0,0 +1,20 @@
+const statement = require('./index');
+const fs = require('fs');
+
+function read(filename) {
+  return fs.readFileSync(filename, 'utf8');
+}
+
+function readJson(filename) {
+  return JSON.parse(read(filename));
+}
+
+test('statement', () => {
+  const invoices = readJson('invoices.json');
+  const plays = readJson('plays.json');
+  const results = readJson('results.json');
+
+  for (let i = 0; i < invoices.length; i++) {
+    expect(statement(invoices[i], plays)).toBe(results[i]);
+  }
+});
