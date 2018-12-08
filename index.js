d86c6d161cf94df9cc76c3e18fd3290053842fb2
Kuan-Ying Chou <kuanyingchou@gmail.com>
2018-12-09 00:25:17 +0800

    slide statement for totalAmount

--- index.js
+++ index.js
@@ -47,16 +47,18 @@ function statement (invoice, plays) {
       return volumeCredits;
   }
 
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
+
+  let totalAmount = 0;
   for (let perf of invoice.performances) {
     totalAmount += amountFor(perf);
   }
+
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
