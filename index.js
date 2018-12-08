d4e42cf297aa11e5c6c95a7d9ee7e9f93153e813
Kuan-Ying Chou <kuanyingchou@gmail.com>
2018-12-09 00:49:13 +0800

    remove parameter invoice

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 function statement (invoice, plays) {
+  const statementData = {};
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
+  return renderPlainText(statementData, plays)
+}
+
+function renderPlainText(data, plays) {
+  let result = `Statement for ${data.customer}\n`;
+
+  for (let perf of data.performances) {
+    // print line for this order
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+  }
+
+  result += `Amount owed is ${usd(totalAmount())}\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
+  return result;  
 
   function amountFor(aPerformance) {
     let result = 0;
@@ -41,7 +58,7 @@ function statement (invoice, plays) {
 
   function totalVolumeCredits() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;
@@ -49,27 +66,11 @@ function statement (invoice, plays) {
 
   function totalAmount() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf);
     }
     return result;
   }
-
-  function renderPlainText(invoice, plays) {
-    let result = `Statement for ${invoice.customer}\n`;
-
-    for (let perf of invoice.performances) {
-      // print line for this order
-      result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-    }
-
-    result += `Amount owed is ${usd(totalAmount())}\n`;
-    result += `You earned ${totalVolumeCredits()} credits\n`;
-    return result;  
-  }
-
-  return renderPlainText(invoice, plays)
-
 }
 
 module.exports = statement;
